1eab59b624121a8b37611e740ebafbc7d854f195
root <root@LAPTOP-83BOB875.localdomain>
2020-07-27 11:51:22 -0300

    Add the calendar component and set a date picker

--- public/appointment-manager/src/main.js
+++ public/appointment-manager/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VCalendar from 'v-calendar';
+
 
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+Vue.use(VCalendar, {
+  componentPrefix: 'vc',  // Use <vc-calendar /> instead of <v-calendar />
+});
